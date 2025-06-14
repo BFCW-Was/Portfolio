1d0639585b1369d52541399b1742bc98f7f79819
BFCW-Was <basticontrewasil@gmail.com>
2025-06-14 01:40:18 -0400

    Update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   // Configuración para Github Pages
   ssr: false,
   app: {
-    baseURL: '/portafolio/',
+    baseURL: '/Portfolio/',
   },
 
   // Internacionalización
