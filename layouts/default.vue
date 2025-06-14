17842d52f087dd39e5c9d231ab7e22a03d0fa447
BFCW-Was <basticontrewasil@gmail.com>
2025-06-14 15:55:04 -0400

    Update README, Logo & Copyright

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,6 @@
         <NuxtRouteAnnouncer />
         <NuxtPage />
         <!-- <Footer /> -->
-        <p>Copyright © 2025 Bastián Contreras Wasilkowski. All rights reserved.</p>
+        <p>Copyright © 2025 by <a style="color: #138583;" href="https://www.linkedin.com/in/basti%C3%A1ncontreraswasilkowski/">Bastián Contreras Wasilkowski</a>. Licensed under <a style="color: #138583;" href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a></p>
     </v-app>
 </template>
--- pages/testing.vue
+++ pages/testing.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <main>
         <div>
             <button @click='isVisibleTextos = !isVisibleTextos'>
                 {{ isVisibleTextos ? 'Ocultar Textos' : 'Mostrar Textos' }}
@@ -7,21 +7,30 @@
             <button @click='isVisibleBotones = !isVisibleBotones'>
                 {{ isVisibleBotones ? 'Ocultar Botones' : 'Mostrar Botones' }}
             </button>
-        </div>
-        <!-- #################### BOTONES #################### -->
-        <div v-if='isVisibleBotones'>
-            <h4>Botones</h4>
+            <button @click='isVisibleImagenes = !isVisibleImagenes'>
+                {{ isVisibleImagenes ? 'Ocultar Imagenes' : 'Mostrar Imagenes' }}
+            </button>
         </div>
         <!-- #################### TEXTOS #################### -->
         <div v-if='isVisibleTextos'>
             <h4>Textos</h4>
         </div>
-    </div>
+        <!-- #################### BOTONES #################### -->
+        <div v-if='isVisibleBotones'>
+            <h4>Botones</h4>
+        </div>
+        <!-- #################### IMÁGENES #################### -->
+        <div v-if='isVisibleImagenes'>
+            <h4>Imagenes</h4>
+            <nuxt-img src="/images/logos/MyPortfolio.png" alt="Logo" />
+        </div>
+    </main>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const isVisibleTextos = ref(true);
+const isVisibleTextos = ref(false);
 const isVisibleBotones = ref(false);
+const isVisibleImagenes = ref(false);
 </script>
